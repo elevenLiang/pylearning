2a676877ed1e4df7c2d2cf6683167b4f0ff04cb1
Liang <15899557862@163.com>
2019-05-06 23:59:20 +0800

    九九乘法表 v2 金字塔形状 by eleven

--- 九九乘法表.py
+++ 九九乘法表.py
@@ -10,12 +10,24 @@ def print_line(line_num):
         print('%s * %s = '%(i,line_num),i * line_num,end=" ")
     print()
     return None
+
+def dengyao(space_num):
+    '''
+    打印空格，变成等腰三角形状
+    :param space_num:
+    :return: None
+    '''
+    print(" " * space_num * 5 , end=" ")
+    return None
+
 def jiujiu():
     '''
     九九乘法表
+    等腰打印
     :return:None
     '''
-    for i in range(1,10):
-        print_line(i)
-
+    for o in range(1,10):
+        dengyao(9 - o)
+        print_line(o)
+    return None
 jiujiu()
